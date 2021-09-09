4e44c4353c694112a7dfb5586be0f8c328223744
padmavati <padmettamshetty@gmail.com>
2021-09-09 13:41:13 +0800

    testingbase

---
+++ javascript crash course/home.js
@@ -0,0 +1,16 @@
+// console.log("hi");
+// alert('hello');
+
+var a="padmavati";
+console.log(a);
+
+var number=09;
+console.log(number);
+
+var age=prompt('what is your age?');
+
+
+document.getElementById("Text").innerHTML= age;
+
+
+
